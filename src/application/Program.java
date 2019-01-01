a0b2e6a8a2179484f1ecb154f0d6091d05a3a0e5
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2019-01-01 18:10:59 -0200

    Inclusao de entities e desenvolvimento do codigo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Pensions;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many rooms will be rent?: ");
+		int n = sc.nextInt();
+		Pensions[] pensions = new Pensions[10];
+		for(int i=1; i<=n; i++) {
+			System.out.println("#Rent" + i);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			pensions[room] = new Pensions(name, email);
+		}
+		for(int i=0; i<pensions.length; i++) {
+			if(pensions[i] != null) {
+				System.out.println(i + ": " + pensions[i].toString());
+			}			
+		}
+		sc.close();
+		
 	}
 
 }
